e6181021118ea479cbff880d108fa7e8b1531a3b
Joel Gustafson <joelg@mit.edu>
2019-12-04 16:42:01 -0500

    context formatting

--- types/context.go
+++ types/context.go
@@ -1,28 +1,25 @@
 package types
 
-const contextURL = "ipfs://bafkreiarfiphrufhuu2pm5uzykmyy6d63tf3wffdqlvkthqhxb4ipkjlau"
+const contextURL = "ipfs://bafkreifcqgsljpst2fabpvmlzcf5fqdthzvhf4imvqvnymk5iifi6mdtru"
 
 const rawContext = `{
 	"@context": {
-    "dcterms": "http://purl.org/dc/terms/",
+		"dcterms": "http://purl.org/dc/terms/",
 		"prov": "http://www.w3.org/ns/prov#",
 		"ldp": "http://www.w3.org/ns/ldp#",
-    "xsd": "http://www.w3.org/2001/XMLSchema#",
-    "dcterms:created": {
-      "@type": "xsd:dateTime"
-    },
-    "dcterms:modified": {
-      "@type": "xsd:dateTime"
-    },
-		"prov:value": {
-      "@type": "@id"
-    },
+		"xsd": "http://www.w3.org/2001/XMLSchema#",
+		"dcterms:created": {
+			"@type": "xsd:dateTime"
+		},
+		"dcterms:modified": {
+			"@type": "xsd:dateTime"
+		},
 		"ldp:membershipResource": {
-      "@type": "@id"
-    },
+			"@type": "@id"
+		},
 		"ldp:hasMemberRelation": {
-      "@type": "@id"
-    }
+			"@type": "@id"
+		}
 	}
 }
 `
